b520091336bce7e990debafc83eff0b8ab3a32c0
damian_kuciel <kuciel.damian2@gmail.com>
2026-09-09 15:20:38 +0200

    feat(config): add appsettings and options class

---
+++ Configuration/AppOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CatFactsApp.Configuration
+{
+    public class AppOptions
+    {
+        public string ApiUrl { get; set; } = string.Empty;
+        public string OutputFilePath { get; set; } = string.Empty;
+    }
+}
