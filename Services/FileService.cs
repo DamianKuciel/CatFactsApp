1ae2b23ca7f012de5e9ec7547f32ef857e899299
damian_kuciel <kuciel.damian2@gmail.com>
2026-09-09 15:41:45 +0200

    feat(file): add file service for saving facts

---
+++ Services/FileService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Options;
+using CatFactsApp.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CatFactsApp.Services
+{
+    public interface IFileService
+    {
+        Task SaveFactToFileAsync(string fact);
+    }
+
+    public class FileService : IFileService
+    {
+        private readonly AppOptions _options;
+
+        public FileService(IOptions<AppOptions> options)
+        {
+            _options = options.Value;
+        }
+
+        public async Task SaveFactToFileAsync(string fact)
+        {
+            var filePath = _options.OutputFilePath;
+            var line = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} - {fact}{Environment.NewLine}";
+            await File.AppendAllTextAsync(filePath, line);
+        }
+    }
+
+}
