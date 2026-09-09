d0a8e18c79452125bc1b89a6c55d596b84b16728
damian_kuciel <kuciel.damian2@gmail.com>
2026-09-09 15:34:06 +0200

    feat(api): add cat fact model and http client

---
+++ Models/CatFactResponse.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CatFactsApp.Models
+{
+    public record CatFactResponse(string Fact, int Length);
+}
---
+++ Services/CatFactClient.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http.Json;
+using CatFactsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CatFactsApp.Services
+{
+    public interface ICatFactClient
+    {
+        Task<CatFactResponse?> GetFactAsync();
+    }
+
+    public class CatFactClient(HttpClient httpClient) : ICatFactClient
+    {
+        public async Task<CatFactResponse?> GetFactAsync() =>
+            await httpClient.GetFromJsonAsync<CatFactResponse>("");
+    }
+}
